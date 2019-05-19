0f46bbd88f8d64000cf3cbf9d1395b725a41a367
Rohan Dongre <dongrerohan421@gmail.com>
2019-05-19 17:32:35 -0400

    rename application

--- src/main/java/com/oauth2/springbootoauth2/SocialApplication.java
+++ src/main/java/com/oauth2/springbootoauth2/SocialApplication.java
@@ -2,12 +2,14 @@ package com.oauth2.springbootoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-public class SpringBootOauth2Application {
+@EnableOAuth2Sso
+public class SocialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOauth2Application.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
 
 }
--- src/test/java/com/oauth2/springbootoauth2/SocialApplicationTests.java
+++ src/test/java/com/oauth2/springbootoauth2/SocialApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootOauth2ApplicationTests {
+public class SocialApplicationTests {
 
 	@Test
 	public void contextLoads() {
